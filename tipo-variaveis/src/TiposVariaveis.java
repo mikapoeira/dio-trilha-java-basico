b5fb3334068bc56015ca05596ccd36e57db292c0
mikael <mikael.r.joaquim@gmail.com>
2024-07-15 22:29:41 -0300

    Commit final tipos de variaveis

--- tipo-variaveis/src/TiposVariaveis.java
+++ tipo-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,14 @@
-public class App {
+public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        short numeroCurto = 1;
+        long numeroNormal = numeroCurto;
+        short numeroCurto2 = (short)numeroNormal;
+
+        int numero = 5;
+        numero = 10;
+
+        final double VALOR_DE_PI = 3.14;
+
+        System.out.println(numero);
     }
 }
